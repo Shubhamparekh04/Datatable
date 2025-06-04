f501c6e50f2b0fcf3c79374fa4c744727ca1543a
Shubhamparekh04 <shubhamparekh04@gmail.com>
2025-06-04 14:12:14 +0530

    Datatable-Axios-Toastify

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,31 +1,54 @@
-import React from 'react'
-
+import React from 'react';
 
 const Form = ({ handleChange, handleSubmit, userObj, isEdit }) => {
-
     return (
-        <>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6 mx-auto p-3">
-                        <form method='post' onSubmit={handleSubmit}>
-                            <div className="mb-3">
-                                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                                <input type="email" value={userObj.email || ""} className="form-control" id="exampleInputEmail1" name='email' onChange={handleChange} />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                                <input type="password" value={userObj.password || ""} className="form-control" id="exampleInputPassword1" name='password' onChange={handleChange} />
-                            </div>
-                            {isEdit?<button type="submit" className="btn btn-warning">Update</button>:<button type="submit" className="btn btn-primary">Submit</button>}
-                            
-                        </form>
+        <div className="container my-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="card shadow-lg rounded-4 border-0">
+                        <div className="card-body p-4">
+                            <h3 className="text-center mb-4 fw-bold text-primary">
+                                {isEdit ? "Update User" : "Register User"}
+                            </h3>
+                            <form onSubmit={handleSubmit}>
+                                <div className="form-floating mb-3">
+                                    <input
+                                        type="email"
+                                        value={userObj.email || ""}
+                                        className="form-control"
+                                        id="floatingEmail"
+                                        name="email"
+                                        onChange={handleChange}
+                                        placeholder="Email address"
+                                        required
+                                    />
+                                    <label htmlFor="floatingEmail">Email address</label>
+                                </div>
 
+                                <div className="form-floating mb-4">
+                                    <input
+                                        type="password"
+                                        value={userObj.password || ""}
+                                        className="form-control"
+                                        id="floatingPassword"
+                                        name="password"
+                                        onChange={handleChange}
+                                        placeholder="Password"
+                                        required
+                                    />
+                                    <label htmlFor="floatingPassword">Password</label>
+                                </div>
+
+                                <button type="submit" className={`btn ${isEdit ? "btn-warning" : "btn-primary"} w-100 py-2`}>
+                                    {isEdit ? "Update" : "Submit"}
+                                </button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </>
-    )
-}
+        </div>
+    );
+};
 
-export default Form
+export default Form;
